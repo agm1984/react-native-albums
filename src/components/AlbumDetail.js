1f93c05e74bd285370630f762fedf7a7bd254e06
Adam Mackintosh <agm1984@gmail.com>
2017-10-01 23:00:04 -0700

    finished app

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,23 +1,46 @@
 import React from 'react'
-import { View, Image, Text } from 'react-native'
+import { View, Image, Text, Linking } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
+import Button from './Button'
 
 // inside Card, props.children = each album
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album
-    const { thumbnailStyle, headerContentStyle } = styles
+    const { title, artist, thumbnail_image, image, url } = album
+    const {
+        thumbnailStyle,
+        thumbnailContainerStyle,
+        headerContentStyle,
+        headerTextStyle,
+        imageStyle
+    } = styles
 
     return (
         <Card>
             <CardSection>
-            <View>
-                <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
-            </View>
-            <View style={headerContentStyle}>
-                <Text>{title}</Text>
-                <Text>{artist}</Text>
-            </View>
+                <View style={thumbnailContainerStyle}>
+                <Image
+                    style={thumbnailStyle}
+                    source={{ uri: thumbnail_image }}
+                />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+
+            <CardSection>
+                <Image
+                    style={imageStyle}
+                    source={{ uri: image }}
+                />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     )
@@ -28,9 +51,23 @@ const styles = {
         height: 50,
         width: 50
     },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 }
 
